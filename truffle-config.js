0a647b3d04e623bd23c29aea1cb57da4d5d9de2f
Catarina Isabel Reis <catarina.isabel.reis@gmail.com>
2020-05-03 20:03:03 +0100

    Small updates to Access by Roles

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,6 +13,15 @@ module.exports = {
     },
     develop: {
       port: 8545
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/1c098fe10174487b92b714e0536479a6`),
+      network_id: 4,       
+      gas: 5500000,
+      gasPrice: 10000000000
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     }
   }
 };
